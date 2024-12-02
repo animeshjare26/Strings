7c32d463cf2db6be09ca3405774b63aa655b49bd
animeshjare26 <animeshjare26@gmail.com>
2024-12-02 16:38:17 +0530

    plaindrom

---
+++ Palindrome.java
@@ -0,0 +1,20 @@
+public class Palindrome {
+    public static boolean isPalimdrome(String str){
+
+        for(int i = 0 ;i < str.length()/2;i++){
+            int n = str.length();
+            if(str.charAt(i) != str.charAt(n-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str1 = "noon";
+        String str2 = "racecar";
+        String str3 = "marks";
+
+        System.out.println(isPalimdrome(str3));
+    }
+}
